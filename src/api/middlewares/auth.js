662233a2dee5346340dc94f200571b635ba8502b
Jikun Liao <liaojikundever@gmail.com>
2020-01-21 03:18:01 -0600

    update

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -12,7 +12,7 @@ const handleJWT = (req, res, next, roles) => async (err, user, info) => {
   const apiError = new APIError({
     message: error ? error.message : 'Unauthorized',
     status: httpStatus.UNAUTHORIZED,
-    stack: error ? error.stack : undefined,
+    stack: error ? error.stack : undefined
   });
 
   try {
@@ -42,13 +42,14 @@ const handleJWT = (req, res, next, roles) => async (err, user, info) => {
 };
 
 exports.ADMIN = ADMIN;
+
 exports.LOGGED_USER = LOGGED_USER;
 
 exports.authorize = (roles = User.roles) => (req, res, next) =>
   passport.authenticate(
-    'jwt', { session: false },
-    handleJWT(req, res, next, roles),
+    'jwt',
+    { session: false },
+    handleJWT(req, res, next, roles)
   )(req, res, next);
 
-exports.oAuth = service =>
-  passport.authenticate(service, { session: false });
+exports.oAuth = (service) => passport.authenticate(service, { session: false });
